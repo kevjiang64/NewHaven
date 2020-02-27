ccb76f12f1a3a4e9f5a442d41f5243d3dc84726a
Adèle Bédard <adele.bedard19@gmail.com>
2020-02-27 16:56:43 -0500

    resources

--- Resources.cpp
+++ Resources.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // HARVEST TILE
 
 HarvestTile::HarvestTile() {
-    topLeft = (int*)malloc(15*sizeof(int));
+    topLeft = (int*)malloc(2*sizeof(int));
     resources = new int[4];
     
     //create counts to keep track of the resources (limited to 3 per harvest tiles)
@@ -59,7 +59,7 @@ HarvestTile::~HarvestTile() {
     delete[] resources;
 };
 
-//DECK FOR HARVEST TILES
+// DECK FOR HARVEST TILES
 
 DeckHarvestTile::DeckHarvestTile() {
 	count = new int(60);
@@ -75,9 +75,9 @@ HarvestTile DeckHarvestTile::draw() {
     if (*count <= 0) {
 		cout << "There is no harvest tile left." << endl;
         
-        //set everything to 0 to return an empty tile
-        newTile.setResources(0);
-        newTile.setTopLeft(0);
+        //set everything to -1 and return an empty tile
+        newTile.setResources(-1);
+        newTile.setTopLeft(-1);
     }
     
     else {
@@ -92,50 +92,40 @@ HarvestTile DeckHarvestTile::draw() {
 Building::Building() {
     int stop = 0;
     
-    //allocate memory (??) for label and number
+	//allocate memory (??) for label and number
     label = (int*)malloc(15*sizeof(int));
     number = (int*)malloc(15*sizeof(int));
-    
+	
+	flipped = (bool*)malloc(sizeof(bool));
+	*flipped = false;
+
     //generate random numbers
     mt19937 rng;
     rng.seed(random_device()());
     uniform_int_distribution<mt19937::result_type> dist;
-    
-    while(!stop) {
-        //generate random numbers for the creation of the building
-        int output = dist(rng) % 4;
-        int numberOutput = dist(rng) % 6 + 1;
+
+    int output = dist(rng) % 4;
+    int numberOutput = dist(rng) % 6 + 1;
         
-        //keep track of the limit of buildings created
-        for(int i=0; i<4;i++) {
-            for(int j=0; j<6; j++) {
-                if((i == output) && (j == numberOutput-1) && (count[i][j] > 6))
-                    continue;
-            }
-        }
-        
-        //add the building created to the count
-        for(int i=0; i<4;i++) {
-            for(int j=0; j<6; j++) {
-                if((i == output) && (j == numberOutput-1))
-                    count[i][j]++;
-            }
-        }
-        
-        *label = output;
-        *number = numberOutput;
-        stop = 1;
-    }
+    *label = output;
+    *number = numberOutput;
 };
 
 Building::~Building() {
 	delete label;
     delete number;
+	delete flipped;
 };
+
+// DECK OF BUILDINGS
 
 DeckBuilding::DeckBuilding() {
 	totalCount = new int(144);
-    count = new int[24];
+    count = new int*[4];
+	
+	for (int i = 0; i < 6; i++) {
+		count[i] = new int[6];
+	}
 };
 
 DeckBuilding::~DeckBuilding() {
@@ -143,24 +133,72 @@ DeckBuilding::~DeckBuilding() {
     delete count;
 };
 
+//keep track of the limit of buildings created
+bool DeckBuilding::validBuilding(Building b) {
+	for (int i = 0; i<4; i++) {
+		for (int j = 0; j<6; j++) {
+			if ((i == b.getLabel()) && (j == b.getNumber() - 1) && (count[i][j] > 6)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+};
+
 Building DeckBuilding::draw() {
-    Building newBuilding;
-    
     if (*totalCount <= 0) {
 		cout << "There is no building left." << endl;
-        newBuilding.setLabel(-1);
-        newBuilding.setNumber(-1);
+		
+		Building emptyBuilding;
+		emptyBuilding.setLabel(-1);
+        emptyBuilding.setNumber(-1);
+		return emptyBuilding;
     }
     
     else {
-        (*totalCount)--;
+		while (1) {
+			Building newBuilding;
+
+			if (validBuilding(newBuilding)) {
+				for (int i = 0; i<4; i++) {
+					for (int j = 0; j<6; j++) {
+						if ((i == newBuilding.getLabel()) && (j == newBuilding.getNumber() - 1)) {
+							count[i][j]++;
+							break;
+						}
+					}
+				}
+
+				(*totalCount)--;
+				return newBuilding;
+			}
+		}
     }
-    
-    return newBuilding;
 };
 
+// HAND
+
+Hand::Hand() {
+	wheatCount = new int;
+	sheepCount = new int;
+	timberCount = new int;
+	stoneCount = new int;
+}
+
+Hand::~Hand() {
+	delete wheatCount;
+	delete sheepCount;
+	delete timberCount;
+	delete stoneCount;
+}
+
+void Hand::exchange() {
+
+}
 
 int main() {
-    DeckBuilding myDeck;
+    Building myDeck;
+	cout << "hello";
     return 0;
 }
