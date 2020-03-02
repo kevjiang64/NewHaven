f2a714d57ec88bd721dcb3850c87fee79ae5e2ab
Justin Teixeira <jteixeira1809@gmail.com>
2020-03-01 22:32:51 -0500

    MapLoader changes

--- GBMapLoaderDriver.cpp
+++ GBMapLoaderDriver.cpp
@@ -22,27 +22,27 @@ int main()
 
     cout << "Test 1:\n";
     //Checking if a VGMap is valid or invalid
-    if (fexists("validMap.txt"))
+    if (checkFile("validMap.txt"))
     {
         numOfPlayers = checkValidPlayerNum("validMap.txt");
         initTile = checkValidTile("validMap.txt");
         cout << "The Number of Players is : " << numOfPlayers <<"\n";
         if (numOfPlayers == 2 || numOfPlayers == 3 || numOfPlayers == 4)
         {
-            cout << "A VGMapLoader Object with a VGMap Object will be created\n\n";
-            VGMapLoader a = VGMapLoader(row, column);
+            cout << "A GBMapLoader Object with a GBMaps Object will be created\n\n";
+            GBMapLoader newMap = GBMapLoader(numOfPlayers, initTile);
         }
     }
 
     cout << "Test 2\n";
     //Checking if a VGMap is valid or invalid
-    if (fexists("invalidMap.txt"))
+    if (checkFile("invalidMap.txt"))
     {
         numOfPlayers = checkValidPlayerNum("invalidMap.txt");
         initTile = checkValidTile("invalidMap.txt");
         if (numOfPlayers == 0)
         {
-            cout << "A VGMapLoader Object will not be created\n";
+            cout << "A GBMapLoader Object will not be created\n";
         }
     }
     return 0;
